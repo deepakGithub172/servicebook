4909a1ccf6f3735877f52c2c36418dd1a159d3f2
Deepak Singh <Deepak Singh@hp>
2019-11-28 01:47:50 +0530

    first commit

---
+++ servicebook/src/main/java/com/example/servicebook/ServicebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicebookApplication.class, args);
+	}
+
+}
---
+++ servicebook/src/test/java/com/example/servicebook/ServicebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
