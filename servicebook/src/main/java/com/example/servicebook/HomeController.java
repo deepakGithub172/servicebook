b5b7e4300525e7f56c1a439cd070a18568424921
deesingh16 <deesingh16@WKWIN7890831.global.publicisgroupe.net>
2019-12-03 18:35:48 +0530

    Added browser pop-up on executing the app.

--- servicebook/src/main/java/com/example/servicebook/HomeController.java
+++ servicebook/src/main/java/com/example/servicebook/HomeController.java
@@ -1,13 +1,24 @@
 package com.example.servicebook;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
-	public String getHomePage() {
-		return "home";
+	@RequestMapping("/home")
+	public ModelAndView getHomePage() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("environments",getEnvironments());
+		mv.setViewName("home");
+		return mv;
+	}
+	
+	private List<String> getEnvironments(){
+		//TODO--
+		 return null;
 	}
 }
--- servicebook/src/main/java/com/example/servicebook/ServicebookApplication.java
+++ servicebook/src/main/java/com/example/servicebook/ServicebookApplication.java
@@ -8,9 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServicebookApplication {
 
+	private static final String HOME_PAGE = "http://localhost:8080/home";
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ServicebookApplication.class, args);
+		openHomePage();
 	}
 
+	private static void openHomePage() throws IOException {
+		Runtime rt = Runtime.getRuntime();
+		rt.exec("rundll32 url.dll,FileProtocolHandler " + HOME_PAGE);
+	}
 }
 
